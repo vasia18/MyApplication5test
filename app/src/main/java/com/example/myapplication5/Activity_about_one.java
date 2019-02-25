77898c8eb93ead66ecb728f48cc6fc5cfacd3847
Arkadiy Ivanov <studiovasia18@gmail.com>
2019-02-25 17:52:12 +0300

    добавлено: Стрелка вернуться назад (понедельник 17:45)

---
+++ app/src/main/java/com/example/myapplication5/Activity_about_one.java
@@ -0,0 +1,13 @@
+package com.example.myapplication5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_about_one extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_one);
+    }
+}
--- app/src/main/java/com/example/myapplication5/Main4Activity.java
+++ app/src/main/java/com/example/myapplication5/Main4Activity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication5;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,13 +19,17 @@ public class Main4Activity extends AppCompatActivity implements View.OnClickList
         buttonFore = (Button) findViewById(R.id.buttonFore);
         buttonFore.setOnClickListener(this);
 
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonFore:
-                Intent intent = new Intent(this, MainActivity.class);
+                Intent intent = new Intent(this, Activity_about_one.class);
                 startActivity(intent);
 
                 break;
