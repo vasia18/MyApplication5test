432108aba27cd5a75f56685fd5ef481bd7b4331e
Arkadiy Ivanov <studiovasia18@gmail.com>
2019-03-05 18:16:04 +0300

    добавлен новый активити Main6Activity (Прописал кнопку назад)

--- app/src/main/java/com/example/myapplication5/Main5Activity.java
+++ app/src/main/java/com/example/myapplication5/Main5Activity.java
@@ -7,22 +7,7 @@ import android.widget.ListView;
 
 public class Main5Activity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5);
 
-        // получаем элемент ListView
-        ListView countriesList = (ListView) findViewById(R.id.countriesList);
 
-        // получаем ресурс
-        String[] countries = getResources().getStringArray(R.array.countries);
-
-        // создаем адаптер
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, countries);
-
-        // устанавливаем для списка адаптер
-        countriesList.setAdapter(adapter);
     }
 }
