4487bb0c36c7b8056c6f910814b515a154ed05f2
Arkadiy Ivanov <studiovasia18@gmail.com>
2019-03-07 17:24:41 +0300

    Много было из измененно и исправленно (приложение загружается)2

--- app/src/main/java/com/example/myapplication5/Main5Activity.java
+++ app/src/main/java/com/example/myapplication5/Main5Activity.java
@@ -1,10 +1,33 @@
 package com.example.myapplication5;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
-public class Main5Activity extends AppCompatActivity {
+public class Main5Activity extends AppCompatActivity implements View.OnClickListener {
 
+    Button buttonFive1;
+    Button buttonFive2;
+
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+
+        buttonFive1 = (Button) findViewById(R.id.buttonFive1);
+        buttonFive1.setOnClickListener(this);
+
+        buttonFive2 = (Button)
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
