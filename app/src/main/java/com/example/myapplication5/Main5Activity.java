31d251cedc9b0cab42f42c5c98fc2fb2f69c2df7
vasia18 <studiovasia18@gmail.com>
2019-03-11 22:25:39 +0300

    добавлен floatingActionButton

--- app/src/main/java/com/example/myapplication5/Main5Activity.java
+++ app/src/main/java/com/example/myapplication5/Main5Activity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication5;
 
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.time.Instant;
+
 public class Main5Activity extends AppCompatActivity implements View.OnClickListener {
 
     Button buttonFive1;
@@ -15,19 +18,37 @@ public class Main5Activity extends AppCompatActivity implements View.OnClickList
 
 
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
 
         buttonFive1 = (Button) findViewById(R.id.buttonFive1);
         buttonFive1.setOnClickListener(this);
 
-        buttonFive2 = (Button)
+        buttonFive2 = (Button) findViewById(R.id.buttonFive2);
+        buttonFive2.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonFive1:
+                Intent intent_1 = new Intent(this, Main6Activity.class);
+                startActivity(intent_1);
+                //Мы написали первое действие вызова кнопки (buttonFive1)
+                break;
+
+            case R.id.buttonFive2:
+                Intent intent_2 = new Intent(this, Main2Activity.class);
+                startActivity(intent_2);
+                //Написали второе действие вызова кнопки (buttonFive2)
+                break;
+            default:
+                break;
+
+        }
+
 
     }
 }
--- app/src/main/java/com/example/myapplication5/Main6Activity.java
+++ app/src/main/java/com/example/myapplication5/Main6Activity.java
@@ -2,12 +2,20 @@ package com.example.myapplication5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Main6Activity extends AppCompatActivity {
+public class Main6Activity extends AppCompatActivity implements View.OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
