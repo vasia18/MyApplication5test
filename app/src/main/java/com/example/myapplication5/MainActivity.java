360bd41dff4892709e4ee4020130fabd4f9c13af
Arkadiy Ivanov <studiovasia18@gmail.com>
2019-03-13 18:27:22 +0300

    новоя редакция в MainActivity

--- app/src/main/java/com/example/myapplication5/MainActivity.java
+++ app/src/main/java/com/example/myapplication5/MainActivity.java
@@ -1,30 +1,59 @@
 package com.example.myapplication5;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
-        setSupportActionBar(mActionBarToolbar);
-    }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(toolbar);
 
-    private void setSupportActionBar(Toolbar mActionBarToolbar) {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("action", null).show();
+            }
+
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
+        // Накачайте меню; добавляйте элементы в панели действий, если он присутствует.
+        getMenuInflater().inflate(R.menu.menu_scroling, menu);
+        return true;
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Элемент панели действий Handle щелкает здесь. Бар действий
+        // автоматическая обработка щелчков по кнопке Home/Up button, так долго
+        // как указать родительскую activity в AndroidManifest.XML.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) ;
         return true;
     }
 
-    MenuItem searchMenuItem = menu.findItem(R.id.action_search);
-    View searchItemView = findViewById(searchMenuItem.getItemId());
+    return super.
+
+    onOptionsItemSelected(item) {
+
+    }
+
+}
 }
